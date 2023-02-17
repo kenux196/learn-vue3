28525b0d2e7e4d6132ab2184200c7df47fb2abea
skyun <skyun@a2tec.co.kr>
2023-02-17 23:42:41 +0900

    TIL 20230217

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { nextTick, reactive } from 'vue';
+
+export default {
+  // data로 묶은것이 반응형이다.
+  data() {
+    return {
+      count: 1,
+      msg: 'Hello World',
+      someObject: {},
+      author: {
+        name: '윤상규',
+        books: [
+          '200자 챌린지만 해도 글쓰기 잘한다.',
+          '내가 중심이 되면 부는 따라온다.',
+          '멈추는 순간 보이는 것들'
+        ]
+      }
+    }
+  },
+  created() {
+    console.log("created()");
+    this.msg = 'CREATED';
+  },
+  mounted() {
+    console.log('mounted(): count = ' + this.count);
+    this.count = 2;
+    this.msg = 'Hello World!!!';
+
+    const newObject = {};
+    this.someObject = newObject;
+
+    console.log(newObject === this.someObject) // false. JavaScript Proxy 객체
+  },
+  computed: {
+    // 계산된 값을 반환하는 속성
+    publishedBooksMessage() {
+      console.log('computed - publishedBooksMessage');
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  },
+  methods: {
+    increamentCount() {
+      this.count++;
+      if (this.count % 2 == 0) {
+        this.msg = 'Hello World!';
+      } else {
+        this.msg = '안녕하세요!!!'
+      }
+      console.log("반응형 데이터 값을 변경하였다.");
+      nextTick(() => {
+        console.log("업데이트된 DOM에 접근 가능하다.");
+      })
+    },
+    calcualteBooksMessage() {
+      console.log('method: calcualteBooksMessage()')
+      return this.author.books.length > 0 ? 'Yes' : 'No';
+    }
+  },
+  setup() {
+    const state = reactive({ name: 'kenux'})
+
+    function changeName() {
+      state.name = 'jiwon'
+    }
+    
+    return {
+      state,
+      changeName
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="container">
+    <h1>{{ msg }}</h1>
+    <p>Count is: {{ count }}</p>
+    <button @click="increamentCount">click me~!</button>
+    <div>{{ state.name }}</div>
+    <button @click="changeName">changeName</button>
+    <p>책을 가지고 있다: <span>{{ publishedBooksMessage }}</span></p>
+    <p>책을 가지고 있다: <span>{{ calcualteBooksMessage() }}</span></p>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/test/proxy.js
@@ -0,0 +1,34 @@
+const target = {
+  message1: "hello",
+  message2: "everyone"
+}
+
+const handler1 = {}
+const proxy1 = new Proxy(target, handler1);
+console.log("proxy1 run ------")
+console.log(proxy1.message1);
+console.log(proxy1.message2);
+
+const handler2 = {
+  get(target, prop, receiver) {
+    return "world";
+  },
+}
+const proxy2 = new Proxy(target, handler2);
+console.log("proxy2 run ------")
+console.log(proxy2.message1);
+console.log(proxy2.message2);
+
+const handler3 = {
+  get(target, prop, receiver) {
+    if (prop === "message2") {
+      return "world";
+    }
+    return Reflect.get(...arguments);
+  },
+}
+const proxy3 = new Proxy(target, handler3);
+console.log("proxy3 run ------")
+console.log(proxy3.message1);
+console.log(proxy3.message2);
+
