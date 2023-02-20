0e377ac88c50277fc82275b6af26e452de3a8330
kenux196 <kenux.yun@gmail.com>
2023-02-20 14:26:13 +0900

    Learn vue3 - provide & inject2

--- src/test/Learn12.vue
+++ src/test/Learn12.vue
@@ -1,11 +1,31 @@
 <script setup>
-import { ref, provide } from 'vue';
+import { ref, provide, readonly } from 'vue';
+import { myInjectionKey } from './keys.js';
 import Child from './Child.vue';
 
 const message = ref('Hello');
-const sayGoobye = ref('Good Bye~!');
 provide('message', message);
+
+const sayGoobye = ref('Good Bye~!');
 provide('goodbye', sayGoobye);
+
+const location = ref('북극');
+function updateLocation() {
+  if (location.value != '남극') {
+    location.value = '남극';
+  } else {
+    location.value = '북극';
+  }
+}
+provide('location', {
+  location,
+  updateLocation,
+});
+
+const count = readonly(100);
+provide(myInjectionKey, {
+  count,
+});
 </script>
 
 <template>
