00495bcb64102666c77c176b17da2eb17765bbdd
kenux196 <kenux.yun@gmail.com>
2023-02-21 11:48:10 +0900

    Add playground app.vue

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import './assets/pico.min.css';
 // import App from './test/Learn11.vue';
 // import App from './test/Learn12.vue';
 // import App from './test/Learn13.vue';
-import App from './test/Router.vue';
+// import App from './test/Router.vue';
+import App from './try/App.vue';
 
 createApp(App).mount('#app');
---
+++ src/try/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+const today = new Date();
+</script>
+
+<template>
+  <main class="container">
+    <div class="grid">
+      <h1 class="page-title">Vue3 Playground</h1>
+      <p>
+        KOREA - {{ today.toLocaleString() }}<br />
+        UTC - {{ today.toUTCString() }}
+      </p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.page-title {
+  color: gray;
+}
+</style>
