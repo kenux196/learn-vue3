0338a106b3843f91ce74bb8685d5434444c32008
kenux196 <kenux.yun@gmail.com>
2023-02-22 12:02:16 +0900

    Add BookForm

---
+++ src/try/components/BookAddForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <dialog :open="isOpen">
+    <article class="dialog-b">
+      <h3>책 등록!</h3>
+      <label for="title">제목</label>
+      <input
+        id="title"
+        type="text"
+        placeholder="제목을 입력하세요"
+        :value="title"
+      />
+      <label for="author">작가</label>
+      <input
+        id="author"
+        type="text"
+        placeholder="작가명 입력"
+        :value="author"
+      />
+      <label for="price">가격</label>
+      <input
+        id="price"
+        type="number"
+        placeholder="제목을 입력하세요"
+        :value="price"
+      />
+      <label for="published-date">발행일</label>
+      <input id="published-date" type="date" :value="publishedDate" />
+      <div class="grid">
+        <a href="#cancel" role="button" class="secondary" @click="close()"
+          >취소</a
+        >
+        <a href="#confirm" role="button" @click="close()">등록</a>
+      </div>
+    </article>
+  </dialog>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const isOpen = ref(false);
+
+function openBookForm() {
+  console.log('Call child openBookForm');
+  isOpen.value = true;
+}
+
+function close() {
+  isOpen.value = false;
+}
+
+// defineExpose를 통하여 함수를 내보낸다.
+defineExpose({
+  openBookForm,
+});
+</script>
+<style scoped>
+.dialog-b {
+  width: 100%;
+}
+</style>
--- src/try/components/BookList.vue
+++ src/try/components/BookList.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, inject, ref } from 'vue';
 import { store } from '../store/store';
 import { Book } from '../js/Book';
+import BookAddForm from './BookAddForm.vue';
 
 const allCheckState = ref(false);
 const isChecked = computed(() => {
@@ -30,6 +31,12 @@ function addBook() {
   );
   repository.save(book);
   alert('새로운 책이 추가되었습니다.');
+}
+
+const bookAddForm = ref('');
+function openBookForm() {
+  console.log('Call parent openBookForm()');
+  bookAddForm.value.openBookForm();
 }
 </script>
 <template>
@@ -67,10 +74,11 @@ function addBook() {
   </table>
   <div class="grid">
     <p role="button" @click="addBook()">add</p>
-    <p role="button">remove</p>
+    <p role="button" @click="openBookForm()">remove</p>
     <p role="button">modify</p>
     <p role="button" class="outline">prev</p>
     <p role="button" class="outline">next</p>
   </div>
+  <BookAddForm ref="bookAddForm" />
 </template>
 <style scoped></style>
