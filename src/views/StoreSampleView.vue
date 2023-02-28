4d5d181a55b01a56d705b39013a464c4ba994707
skyun <skyun@a2tec.co.kr>
2023-02-28 21:39:45 +0900

    Add input for user infor

--- src/views/StoreSampleView.vue
+++ src/views/StoreSampleView.vue
@@ -13,6 +13,7 @@ const userStore = useUserStore();
 // 반응형을 유지하면서 스토어에서 속성 추출하기: storeToRefs() 사용
 // 스토어의 상태만 사용하고, 액션을 호출하지 않을 때 유용한 방법이다.
 const { name, count, doubleCount } = storeToRefs(counterStore);
+const { accessToken, firstName, lastName } = storeToRefs(userStore);
 </script>
 <template>
   <div class="grid">
@@ -22,10 +23,14 @@ const { name, count, doubleCount } = storeToRefs(counterStore);
     <button @click="counterStore.increment">count 증가</button>
   </div>
   <div>
-    <p>accessToken: {{ userStore.$state.accessToken }}</p>
     <p>
-      user Info: {{ userStore.$state.firstName }}
-      {{ userStore.$state.lastName }}
+      accessToken: {{ accessToken }} <br />
+      user name: {{ firstName }} {{ lastName }}
     </p>
+    <div class="grid">
+      <input type="text" v-model="lastName" />
+      <input type="text" v-model="firstName" />
+      <input type="text" v-model="accessToken" />
+    </div>
   </div>
 </template>
