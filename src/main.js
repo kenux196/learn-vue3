154da63e73f15d28fb6a9269d873281eb21d0faf
kenux196 <kenux.yun@gmail.com>
2024-02-21 16:04:37 +0900

    vue3 ref & reactive basic

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ const pinia = createPinia();
 pinia.use(piniaPersist);
 const app = createApp(App);
 
+// app 레벨의 에러 핸들링 정의 예시
+app.config.errorHandler = (err) => {
+  // console.log(err);
+};
+
 app.use(pinia);
 app.use(router);
+
+// 컴포넌트 전역 등록 예시
+// app.component('TodoDeleteButton', TodoDeleteButton);
+
 app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
 <script setup>
+import { reactive, ref } from 'vue';
 import WelcomeMessage from '../components/WelcomeMessage.vue';
 
 const today = new Date();
+const showMessage = ref(false);
+const someUrl = ref('https://www.naver.com');
+const count = ref(0);
+
+const person = {
+  name: 'kenux',
+  age: 0,
+};
+const state = reactive({ number: 0 });
+const personProxy = reactive(person);
+// const primitive = reactive(1); // reactive는 기본형 미지원. only 객체, 배열, 컬렉션 타입만 가능
+
+function showHiddenMessage() {
+  console.log('링크를 눌렀네요~~~');
+  showMessage.value = !showMessage.value;
+  count.value++;
+  person.age++;
+}
 </script>
 
 <template>
@@ -14,6 +33,16 @@ const today = new Date();
       </p>
     </div>
     <WelcomeMessage />
+    <a v-on:click="showHiddenMessage()">여기를 누르면? {{ count }}</a>
+    <p v-if="showMessage">오늘은 기분이 어떤가요?</p>
+    <div>
+      <a v-bind:href="someUrl">네이버 : {{ someUrl }}</a>
+    </div>
+    <button @click="state.number++">대기번호(+): {{ state.number }}</button>
+    <button @click="state.number--">대기번호(-): {{ state.number }}</button>
+    <button @click="personProxy.age++">
+      {{ personProxy.name }}: {{ personProxy.age }}
+    </button>
   </main>
 </template>
 
