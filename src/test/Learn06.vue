fd273e9ac14601df23e179e760b26e3c2b290444
kenux196 <kenux.yun@gmail.com>
2024-02-22 13:12:08 +0900

    modify learn06.vue

--- src/test/Learn06.vue
+++ src/test/Learn06.vue
@@ -4,19 +4,22 @@ import { ref, watch } from 'vue';
 const question = ref('');
 const answer = ref('질문에는 일반적으로 물음표가 포함됩니다.');
 const imgUrl = ref('');
+const loading = ref(false);
 
 watch(question, async (newQuestion, oldQuestion) => {
   if (newQuestion.indexOf('?') > -1) {
+    loading.value = true;
     answer.value = '생각중...';
     try {
-      const body = await fetch('https://yesno.wtf/api').then((res) => {
-        return res.json();
-      });
+      const res = await fetch('https://yesno.wtf/api');
+      const body = await res.json();
       console.log(body);
       answer.value = body.answer === 'yes' ? '네' : '아니오';
       imgUrl.value = body.image;
     } catch (error) {
       answer.value = '에러! API에 연결할 수 없습니다. ' + error;
+    } finally {
+      loading.value = false;
     }
   }
 });
@@ -27,9 +30,7 @@ watch(question, async (newQuestion, oldQuestion) => {
     <h1>감시자</h1>
     <p>
       예/아니오 질문:
-      <span>
-        <input v-model="question" />
-      </span>
+      <input v-model="question" :disabled="loading" />
     </p>
     <p>대답: {{ answer }}</p>
     <img v-bind:src="imgUrl" />
