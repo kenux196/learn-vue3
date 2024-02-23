df501eabe8bb78ee5e1096040e91af2e220bde90
kenux196 <kenux.yun@gmail.com>
2024-02-23 17:05:08 +0900

    vue3 basic sample component

---
+++ src/test/Sample05.vue
@@ -0,0 +1,21 @@
+<!-- Simple Component -->
+<script setup>
+import { ref } from 'vue';
+import SampleTodoItem from './SampleTodoItem.vue';
+
+const groceryList = ref([
+  { id: 0, text: 'Vegetables' },
+  { id: 1, text: 'Cheese' },
+  { id: 2, text: 'Whatever else humans are supposed to eat' },
+]);
+</script>
+<template>
+  <h2>Simple Component</h2>
+  <ol>
+    <SampleTodoItem
+      v-for="item in groceryList"
+      :todo="item"
+      :key="item.id"
+    ></SampleTodoItem>
+  </ol>
+</template>
