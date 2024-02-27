161986d26a2c54b4ac34f470ec714fefe3366607
kenux196 <kenux.yun@gmail.com>
2024-02-27 17:57:13 +0900

    api test app

--- src/views/ApiTestView.vue
+++ src/views/ApiTestView.vue
@@ -1,12 +1,18 @@
 <script setup>
 // const jsonData = require('../json/test.json');
 // console.log(jsonData);
-fetch('../json/test.json')
-  .then((res) => {
-    return res.json();
-  })
-  .then((jsonData) => console.log(jsonData));
+// fetch('../json/test.json')
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((jsonData) => console.log(jsonData));
+import test from '@/assets/test.json';
+import { ref } from 'vue';
+
+const testList = ref(test);
+console.log(testList);
 </script>
 <template>
   <h1>API Test App</h1>
+  <p v-for="item in testList">{{ item.name }}</p>
 </template>
