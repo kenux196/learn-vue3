228b8d0f38c7fb86d11c5aad55d0686a828026f4
kenux196 <kenux.yun@gmail.com>
2024-06-08 01:37:34 +0900

    modify book page

--- src/components/book/BookList.vue
+++ src/components/book/BookList.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="grid">
-    <p style="font-size: xx-large">보유 도서 목록</p>
-    <p role="button" @click="openBookForm()">add</p>
-    <p role="button" @click="removeBooks()">remove</p>
-    <p role="button" @click="modifyBook()">modify</p>
+  <div class="text-h5">책</div>
+  <div class="q-gutter-md">
+    <q-btn color="primary" @click="openBookForm" class="q-mx-sm">추가</q-btn>
+    <q-btn color="primary" @click="removeBooks" class="q-mx-sm">삭제</q-btn>
+    <q-btn color="primary" @click="modifyBook" class="q-mx-sm">수정</q-btn>
+  </div>
+  <div class="q-my-md">
+    <q-table
+      ref="tableRef"
+      title="책 리스트"
+      :rows="rows"
+      :columns="columns"
+      row-key="id"
+      selection="multiple"
+      v-model:selected="selected"
+      @selection="handleSelection"
+    >
+      <template v-slot:header-selection="scope">
+        <q-checkbox v-model="scope.selected" />
+      </template>
+      <template v-slot:body-selection="scope">
+        <q-checkbox
+          :model-value="scope.selected"
+          @update:model-value="
+            (val, evt) => {
+              Object.getOwnPropertyDescriptor(scope, 'selected').set(val, evt);
+            }
+          "
+        />
+      </template>
+    </q-table>
   </div>
   <table role="grid">
     <thead>
@@ -42,7 +68,7 @@
   <Progress ref="progress" />
 </template>
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, toRaw, nextTick } from 'vue';
 import { store } from '@/stores/store';
 import BookAddForm from './BookAddForm.vue';
 import Progress from '../common/Progress.vue';
@@ -56,6 +82,69 @@ const state = reactive({
 
 state.books = getBooks();
 
+const tableRef = ref();
+const selected = ref([]);
+let storedSelectedRow;
+
+const columns = [
+  {
+    name: 'id',
+    required: true,
+    label: 'no.',
+    align: 'left',
+    field: (row) => row.id,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  { name: 'title', align: 'center', label: '제목', field: 'title', sortable: true },
+  { name: 'author', align: 'center', label: '저자', field: 'author', sortable: true },
+  { name: 'price', align: 'center', label: '가격', field: 'price' },
+  { name: 'publishDate', align: 'center', label: '발행일', field: 'publishDate' },
+];
+const rows = getBooks();
+
+function handleSelection({ rows, added, evt }) {
+  // ignore selection change from header of not from a direct click event
+  if (rows.length !== 1 || evt === void 0) {
+    return;
+  }
+
+  const oldSelectedRow = storedSelectedRow;
+  const [newSelectedRow] = rows;
+  const { ctrlKey, shiftKey } = evt;
+
+  if (shiftKey !== true) {
+    storedSelectedRow = newSelectedRow;
+  }
+
+  // wait for the default selection to be performed
+  nextTick(() => {
+    if (shiftKey === true) {
+      const tableRows = tableRef.value.filteredSortedRows;
+      let firstIndex = tableRows.indexOf(oldSelectedRow);
+      let lastIndex = tableRows.indexOf(newSelectedRow);
+
+      if (firstIndex < 0) {
+        firstIndex = 0;
+      }
+
+      if (firstIndex > lastIndex) {
+        [firstIndex, lastIndex] = [lastIndex, firstIndex];
+      }
+
+      const rangeRows = tableRows.slice(firstIndex, lastIndex + 1);
+      // we need the original row object so we can match them against the rows in range
+      const selectedRows = selected.value.map(toRaw);
+
+      selected.value =
+        added === true
+          ? selectedRows.concat(rangeRows.filter((row) => selectedRows.includes(row) === false))
+          : selectedRows.filter((row) => rangeRows.includes(row) === false);
+    } else if (ctrlKey !== true && added === true) {
+      selected.value = [newSelectedRow];
+    }
+  });
+}
 function selectAll() {
   console.log('[debug] updateChecked');
   if (state.selectedAll) {
