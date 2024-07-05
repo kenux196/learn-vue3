e12d7976927c9d0fb03267058593fa31d876c7ca
kenux196 <kenux.yun@gmail.com>
2024-07-05 20:44:03 +0900

    Add winston logger

--- backend/logger/logger.js
+++ backend/logger/logger.js
@@ -1,8 +1,37 @@
-import morgan from 'morgan';
+import winston from 'winston';
 
-const LOG_FORMAT =
-  '[:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
+const { combine, timestamp, printf, colorize } = winston.format;
 
-const logger = morgan(LOG_FORMAT);
+const logFormat = printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
 
-export default logger;
+const logger = winston.createLogger({
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    logFormat
+  ),
+});
+
+logger.stream = {
+  write: (message) => {
+    logger.info(message);
+  },
+};
+
+// Production 환경이 아닌 경우(dev 등) 배포 환경에서는
+// 최대한 자원을 안잡아 먹는 로그를 출력해야함
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: combine(
+        colorize({ all: true }), // console 에 출력할 로그 컬러 설정 적용함
+        logFormat // log format 적용
+      ),
+    })
+  );
+}
+
+export { logger };
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import path from 'node:path';
 import history from 'connect-history-api-fallback';
-import logger from './logger/logger.js';
+import morgan from 'morgan';
+import { logger } from './logger/logger.js';
 
 import indexRouter from './router/index.js';
 import birdsRouter from './router/birds.js';
 
 const port = process.env.SERVER_PORT || 3000;
+const LOG_FORMAT = ':method :url :status :response-time(ms)';
 
 const app = express();
 
 // config
 app
+  .use(morgan(LOG_FORMAT, { stream: logger.stream }))
   .use(history())
-  .use(logger)
   .use('/', express.static(path.resolve() + '/public'));
 
 // routers
@@ -21,5 +23,8 @@ app.use('/', indexRouter);
 app.use('/birds', birdsRouter);
 
 app.listen(port, () => {
+  logger.info('info log');
+  logger.warn('warn log');
+  logger.error('error log');
   console.log(`Server is running on port: ${port}`);
 });
