92eb21425b88fa9e954fed7ffd0626c06dc72eae
kenux196 <kenux.yun@gmail.com>
2024-07-25 23:21:05 +0900

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,37 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  return defineConfig({
-    plugins: [
-      vue({
-        template: { transformAssetUrls },
-      }),
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
 
-      // @quasar/plugin-vite options list:
-      // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
-      quasar({
-        sassVariables: 'src/css/quasar.variables.scss',
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: 'src/css/quasar.variables.scss',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: './dist',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    build: {
-      outDir: './dist',
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-  });
-};
+  },
+});
