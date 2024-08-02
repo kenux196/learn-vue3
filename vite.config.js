6752da3f55bb771d9ef72a0e0318e2fb4ce33c7e
kenux196 <kenux.yun@gmail.com>
2024-08-03 00:14:17 +0900

    move quasar.variables.scss file

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default ({ mode }) => {
       // @quasar/plugin-vite options list:
       // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
       quasar({
-        sassVariables: 'src/css/quasar.variables.scss',
+        sassVariables: '@/quasar.variables.scss',
       }),
     ],
     resolve: {
