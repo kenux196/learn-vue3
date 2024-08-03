d4a3ede695ea68fc202dd2e207fd47f0dfed3693
kenux196 <kenux.yun@gmail.com>
2024-08-03 17:37:57 +0900

    modify aoubt.vue

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,11 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="@/assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px" />
+  <q-page padding="">
+    <section>
+      <div class="" style="background-color: greenyellow">
+        <h5>기술 스택</h5>
+        <ul>
+          <li>Vue3</li>
+          <li>vue router</li>
+          <li>pinia</li>
+          <li>Vite</li>
+          <li>Quasar UI Framework</li>
+          <li>i18n</li>
+        </ul>
+      </div>
+    </section>
+    <img
+      alt="Quasar logo"
+      src="@/assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage',
-});
-</script>
+<script setup></script>
