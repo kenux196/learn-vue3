c1377263d8a12593207ddfaa1da10bd0f02dc1a0
kenux196 <kenux.yun@gmail.com>
2024-08-06 10:13:18 +0900

    update packages version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,51 +17,43 @@ module.exports = {
   },
 
   // Rules order is important, please avoid shuffling them
-  extends: [
-    // Base ESLint recommended rules
+  'extends': [
+    'plugin:vue/vue3-essential',
     'eslint:recommended',
-
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
     // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
     // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
-    // https://github.com/prettier/eslint-config-prettier#installation
-    // usage with Prettier, provided by 'eslint-config-prettier'.
-    'prettier',
+    // 'prettier',
     '@vue/eslint-config-prettier/skip-formatting',
   ],
 
-  plugins: [
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
-    // required to lint *.vue files
-    'vue',
+  // plugins: [
+  //   // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
+  //   // required to lint *.vue files
+  //   'vue',
 
-    // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
-    // Prettier has not been included as plugin to avoid performance impact
-    // add it as an extension for your IDE
-  ],
+  //   // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
+  //   // Prettier has not been included as plugin to avoid performance impact
+  //   // add it as an extension for your IDE
+  // ],
 
-  globals: {
-    ga: 'readonly', // Google Analytics
-    cordova: 'readonly',
-    __statics: 'readonly',
-    __QUASAR_SSR__: 'readonly',
-    __QUASAR_SSR_SERVER__: 'readonly',
-    __QUASAR_SSR_CLIENT__: 'readonly',
-    __QUASAR_SSR_PWA__: 'readonly',
-    process: 'readonly',
-    Capacitor: 'readonly',
-    chrome: 'readonly',
-  },
+  // globals: {
+  //   ga: 'readonly', // Google Analytics
+  //   cordova: 'readonly',
+  //   __statics: 'readonly',
+  //   __QUASAR_SSR__: 'readonly',
+  //   __QUASAR_SSR_SERVER__: 'readonly',
+  //   __QUASAR_SSR_CLIENT__: 'readonly',
+  //   __QUASAR_SSR_PWA__: 'readonly',
+  //   process: 'readonly',
+  //   Capacitor: 'readonly',
+  //   chrome: 'readonly',
+  // },
 
-  // add your custom rules here
-  rules: {
-    'prefer-promise-reject-errors': 'off',
+  // // add your custom rules here
+  // rules: {
+  //   'prefer-promise-reject-errors': 'off',
 
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
+  //   // allow debugger during development only
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  // },
 };
