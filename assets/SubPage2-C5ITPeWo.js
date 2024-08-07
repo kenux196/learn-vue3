d3b919de209870014b55fc9e0740f79daa30b89a
kenux196 <kenux196@users.noreply.github.com>
2024-08-07 14:19:25 +0000

    deploy: c8ad74b681f53cc9c87241b09d5b8a5d0e5e0cf7

---
+++ assets/SubPage2-C5ITPeWo.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,c as o}from"./index-Bak3fYKI.js";const r={};function t(n,a){return c(),o("div",null,"Subpage 2")}const f=e(r,[["render",t]]);export{f as default};
