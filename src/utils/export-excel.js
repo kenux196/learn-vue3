f9378cbc27a59f653f70fbccc08d6f1e37e90a8e
kenux196 <kenux.yun@gmail.com>
2024-08-10 10:45:09 +0900

    excel download util function

--- src/utils/export-excel.js
+++ src/utils/export-excel.js
@@ -3,10 +3,24 @@ import xlsx from 'xlsx';
 /**
  * json 데이터를 excel 파일로 변환하는 유틸함수
  *
+ * const jsonData = {
+ *  header: [['title', 'author']],
+ *  body: [
+ *    {
+ *       title: 'some book1',
+ *       author: 'writer1',
+ *     },
+ *     {
+ *        title: 'some book2',
+ *        author: 'writer2',
+ *     },
+ *   ],
+ * };
+ *
  * @param {string} sheetName
  * @param {string} fileName
  * @param {Array} body
- * @param {[string[]]} [header=null]
+ * @param {Array<string[]>} [header=null]
  */
 function exportToExcel(sheetName, fileName, body, header = null) {
   const workBook = xlsx.utils.book_new();
