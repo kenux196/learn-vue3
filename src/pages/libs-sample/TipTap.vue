87e7d1aee790a5fc0551aadc5d13e5d66b53ffaf
kenux196 <kenux.yun@gmail.com>
2024-08-15 08:21:37 +0900

    add smaple for tiptap

--- src/pages/libs-sample/TipTap.vue
+++ src/pages/libs-sample/TipTap.vue
@@ -1,6 +1,14 @@
 <template>
   <div>component</div>
-  <EditorContent :editor="editor" />
+  <button
+    @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+    :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+  >
+    H1
+  </button>
+  <div class="editor_container">
+    <EditorContent :editor="editor" />
+  </div>
 </template>
 
 <script setup>
@@ -8,9 +16,18 @@ import StarterKit from '@tiptap/starter-kit';
 import { EditorContent, useEditor } from '@tiptap/vue-3';
 
 const editor = useEditor({
-  constent: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
   extensions: [StarterKit],
+  constent: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.editor_container {
+  height: 300px;
+  width: 100%;
+  background-color: beige;
+  border: 1px;
+  border-radius: 8px;
+  border-style: solid;
+}
+</style>
